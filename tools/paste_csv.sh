00f8ab089859d59076014468a0cd7aa362f354e2
ai_var_ <none>
2019-10-08 12:33:14 +0900

    2nd week

--- tools/paste_csv.sh
+++ tools/paste_csv.sh
@@ -1,1 +1,1 @@
-paste ../trainee_info.csv <(sh get_rank.sh | awk -F "," '{print $2}') | awk -F "\t" '{print $1 $2}' | awk -F "," '{print $1 "," $2 "," $3 ",,,f," $7}'
+paste ../trainee_info.csv <(sh get_rank.sh | awk -F "," '{print $2}') | awk -F "\t" '{print $1 "," $2}' | awk -F "," '{print $1 "," $2 "," $3 ",f," $6}'
